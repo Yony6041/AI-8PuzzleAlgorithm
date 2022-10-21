b725de25b36184e50a56240fd0a3757572f5010f
Yony6041 <dercomputerschamane@gmail.com>
2022-10-21 01:40:36 -0500

    debugging

--- src/algorithms.py
+++ src/algorithms.py
@@ -11,24 +11,21 @@ MaxFrontier= 0  # max frontier
 
 # Depth First Search *****************************************************
 def dfs(environment):
-    global GoalState, GoalNode, MaxSearchDeep, MaxFrontier
+    global GoalState, GoalNode, MaxSearchDeep, MaxFrontier, initial_state
     boardVisited = set()
     stack = list([environment])
+    
+    initial_state = environment.state
     while stack:
         global GoalState
         node = stack.pop()
         boardVisited.add(node.map)
+        print("What we want:", GoalState)
+        print("What we have:", node.state)
         if node.state == GoalState:
             GoalNode = node
             return stack
         #inverse the order of next paths for execution porpuses
-        
-        # Me dice que node son dos argumentos 
-        
-        
-        # File "/home/schamane/unam/ai/AI-8PuzzleAlgorithm/src/algorithms.py", line 26, in dfs
-        # reversedPaths = environment.subNodes(node)
-        # TypeError: Environment.subNodes() takes 1 positional argument but 2 were given
         print("node:", node.depth)
         posiblePaths = reversed(environment.subNodes(node.state, node, node.depth))
         for path in posiblePaths:
